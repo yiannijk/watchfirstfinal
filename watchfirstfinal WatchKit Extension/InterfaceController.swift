bf2612f77a98fed35a488aacd634eade6bdf9b03
Kontorousis, Jonathan <dimitre@ugl11.ccs.itd.umich.edu>
2016-02-14 20:39:24 -0500

    Initial Commit

---
+++ watchfirstfinal WatchKit Extension/InterfaceController.swift
@@ -0,0 +1,31 @@
+//
+//  InterfaceController.swift
+//  watchfirstfinal WatchKit Extension
+//
+//  Created by Kontorousis, Jonathan on 2/14/16.
+//  Copyright © 2016 Kontorousis, Jonathan. All rights reserved.
+//
+
+import WatchKit
+import Foundation
+
+
+class InterfaceController: WKInterfaceController {
+
+    override func awakeWithContext(context: AnyObject?) {
+        super.awakeWithContext(context)
+        
+        // Configure interface objects here.
+    }
+
+    override func willActivate() {
+        // This method is called when watch view controller is about to be visible to user
+        super.willActivate()
+    }
+
+    override func didDeactivate() {
+        // This method is called when watch view controller is no longer visible
+        super.didDeactivate()
+    }
+
+}
